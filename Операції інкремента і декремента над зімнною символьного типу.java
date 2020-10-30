ad903c188ba615930d8a62bc82349518becccf2e
Roman Hera <romahera6@gmail.com>
2020-10-30 19:11:30 +0200

    Add files via upload

---
+++ Операції інкремента і декремента над зімнною символьного типу.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'L';
+        System.out.println("ch = " + ch);
+
+        ch++;
+        System.out.println("ch zminilos na " +ch);
+
+        ch = 'D';
+        ch--;
+        System.out.println("Znachenya ch znowu zminilos i rivne na " + ch);
+    }
+}
