cbaeed156bd47c1685bf8f71fe4d144da1149598
Roman Hera <romahera6@gmail.com>
2020-10-31 19:06:10 +0200

    Add files via upload

---
+++ Приклад цикла while.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char ch = 'а';
+        while (ch <= 'я') {
+            System.out.println(ch);
+            ch++;
+        } //while
+    }
+}
